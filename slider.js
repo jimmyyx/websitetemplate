a909679db6d3e34d693838acc2301d780b265362
unknown <xujimmyy@gmail.com>
2017-07-06 15:47:02 -0400

    navbar sticks to bottom on mobile devices

--- slider.js
+++ slider.js
@@ -4,9 +4,10 @@ function check(){
         var bottom_of_window = $(window).scrollTop() + $(window).height();
         if( bottom_of_window > bottom_of_object ){
             $(this).css("opacity", "1");
-        }else if (bottom_of_window < $(this).offset().top){
-            $(this).css("opacity", "0");
         }
+        // else if (bottom_of_window < $(this).offset().top){
+        //     $(this).css("opacity", "0");
+        // }
     }); 
 }
 
